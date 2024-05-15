e6f3b77aaf106169bb69ca22012fcc595abca703
justinn1104 <just.andres1104@gmail.com>
2024-05-15 04:15:51 -0500

    JAVA

---
+++ P1Tarea1ApacheMendozaJustin/src/p1tarea1apachemendozajustin/P1Tarea1ApacheMendozaJustin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1tarea1apachemendozajustin;
+
+/**
+ *
+ * @author mendo
+ */
+public class P1Tarea1ApacheMendozaJustin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
